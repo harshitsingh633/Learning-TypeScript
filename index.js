43ed2cf01f955a0ac9054f2e6fb22f2bb3036c53
Harshit singh <hs8700882@gmail.com>
2025-03-05 12:05:17 +0530

    Greet function

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let x = 1; //Initializing the x variable to be a integer 
+console.log(x);
+let y = "Hi there";
+console.log(y);
---
+++ index.ts
@@ -0,0 +1,14 @@
+let x : number = 1; //Initializing the x variable to be a integer 
+console.log(x);
+
+let y : String = "Hi there"
+console.log(y);
+
+
+// A function that greets a user given their first name
+
+function greet(firstName : String){
+    console.log("Hello"+ firstName)
+}
+
+greet("Harshit")
